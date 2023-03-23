4b1f42fa44a36fe90c81a8324a80f36e306cb17a
llSaiMonDll <zerotwotv@gmail.com>
2023-03-23 21:31:19 +0700

    ReactMUI

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return (
+        <div className="layout">
+            <img src="assets/mond.jpg" alt="profile" />
+            <h1 className="name">Kampol Suwannatam</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
